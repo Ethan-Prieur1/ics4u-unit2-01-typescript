2bea39b205e7e6d86a2d1d09be90eab90fbdf882
Ethan-Prieur1 <ethan.prieur@mths.ca>
2022-10-04 14:04:40 +0000

    For Michael

---
+++ index.ts
@@ -0,0 +1,8 @@
+/**
+ * This is the standard index.ts starting point.
+ *
+ * author  Ethan Prieur
+ * version 1.0
+ * since   2022-10-04
+ */
+import MrCoxallStack from '.mrcoxallstack'
---
+++ src/stack.ts
@@ -0,0 +1,21 @@
+/**
+ * This class creates a stack
+ *
+ * By:      Ethan Prieur
+ * Version: 1.0
+ * Since:   2022-10-04
+ */
+
+class Stack {
+  stackArray: number[] = []
+
+  push (pushedNumber: number): void {
+    // add a number to the array
+    this.stackArray.push(pushedNumber)
+  }
+
+  showStack (): void {
+    console.log(this.stackArray)
+  }
+}
+export = Stack
